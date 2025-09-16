a9c73908935ec189929ae5562177784bece5c9c7
quicksand <mock@mail.com>
2025-09-17 00:42:59 +0200

    run server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build routes
+    let app = Router::new().route("/", get(root));
+
+    // address & port
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on http://{}", addr);
+
+    // run server
+    axum_server::bind(addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, Axum!"
 }
